3364a427d4ac7db04b915e69b08657c84a8454f0
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2023-11-15 15:51:00 +0100

    Update PreattivazioneISO.ps1

--- PreattivazioneISO/PreattivazioneISO.ps1
+++ PreattivazioneISO/PreattivazioneISO.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Output "Necessita di essere lanciato come Amministratore. Riavvio."
-    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm -Uri https://bit.ly/wincustomizergui | iex"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm -Uri https://bit.ly/preattivazioneiso | iex"
     break
 }
 
